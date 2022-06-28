d777407a10df903b7f8748acca25188121d7d3c5
PauloAlves8039 <pj38alves@gmail.com>
2022-06-28 08:46:32 -0300

    Criando estrutura do primeiro exercício

---
+++ src/01-Exercicio/Classes/Cliente.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace _01_Exercicio.Classes
+{
+    public class Cliente
+    {
+        public string Nome { get; set; }
+        public string Pais { get; set; }
+        public string Email { get; set; }
+
+        public static List<Cliente> ObtendoListaDeClientes() 
+        {
+            var listaDeClientes = new List<Cliente>() 
+            {
+                new Cliente{ Nome="Pedro", Pais="Brasil", Email="pedro@email.com" },
+                new Cliente{ Nome="Florencia", Pais="Chile", Email="maria@email.com" },
+                new Cliente{ Nome="Florencia", Pais="Chile", Email="maria@email.com" },
+                new Cliente{ Nome="Sofia", Pais="Paraguai", Email="paraguai@email.com" }
+            };
+
+            return listaDeClientes;
+        }
+    }
+}
---
+++ src/01-Exercicio/Classes/LocalizarCliente.cs
@@ -0,0 +1,17 @@
+﻿namespace _01_Exercicio.Classes
+{
+    public class LocalizarCliente
+    {
+        public static Cliente PesquisarPorPais(string pais) 
+        {
+            var resultado = Cliente.ObtendoListaDeClientes().Find(c => c.Pais == pais);
+            return resultado;
+        }
+
+        public static Cliente PesquisarPorNome(string nome) 
+        {
+            var resultado = Cliente.ObtendoListaDeClientes().Find(c => c.Nome == nome);
+            return resultado;
+        }
+    }
+}
