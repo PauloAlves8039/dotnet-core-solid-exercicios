edf9cdbeada5056698b9c2b93bcabc0cb658d2b1
PauloAlves8039 <pj38alves@gmail.com>
2022-06-30 08:21:12 -0300

    Criando estrutura do terceiro exercício

---
+++ src/03-Exercicio/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _03_Exercicio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** EXERCÍCIO 01 *****");
+
+            Console.ReadLine();
+        }
+    }
+}
