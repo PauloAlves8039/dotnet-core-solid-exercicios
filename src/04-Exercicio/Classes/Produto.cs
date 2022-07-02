fbe06be786f5557382043e8d4b0781e2e342e9c8
PauloAlves8039 <pj38alves@gmail.com>
2022-07-02 10:06:52 -0300

    Criando estrutura do quarto exercício

---
+++ src/04-Exercicio/Classes/Produto.cs
@@ -0,0 +1,8 @@
+﻿namespace _04_Exercicio.Classes
+{
+    public class Produto
+    {
+        public string Nome { get; set; }
+        public decimal Preco { get; set; }
+    }
+}
---
+++ src/04-Exercicio/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _04_Exercicio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** EXERCÍCIO 03 *****");
+
+            Console.ReadLine();
+        }
+    }
+}
